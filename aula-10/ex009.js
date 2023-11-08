51b0541e9b809b1273164e3395dce337d9ad0193
Leonardo Gomes <devleonardo96@gmail.com>
2023-11-07 23:22:38 -0300

    add

---
+++ aula-10/ex009.js
@@ -0,0 +1,13 @@
+var pais = "Brasil";
+console.log(`Vivendo em ${pais}`);
+
+// condição composta
+
+if (pais == "Brasil") {
+  console.log("Você é Brasileiro!");
+} else {
+  console.log("Você éEstrangeiro");
+}
+
+
+ // fazer este exercício em forma de site html
