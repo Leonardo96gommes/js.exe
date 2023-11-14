e4ad9c87384598ddb44784454817fa96e19d58f4
Leonardo Gomes <devleonardo96@gmail.com>
2023-11-13 21:24:06 -0300

    at

---
+++ aula-11/ex011.js
@@ -0,0 +1,11 @@
+var idade = 16;
+
+if (idade < 16) {
+  console.log("Não vota");
+} else if (idade < 18) {
+  // Se a idade for maior ou igual a 16 e menor que 18
+  console.log("Voto opcional");
+} else {
+  // Se a idade for 18 ou mais
+  console.log("Voto obrigatório")
+}
