5bb0b4d90c846bb416e63602503328f534204b7c
Leonardo Gomes <devleonardo96@gmail.com>
2023-11-13 22:06:40 -0300

    att

--- aula-11/ex015.js
+++ aula-11/ex015.js
@@ -1,8 +1,8 @@
-var idade = 16;
-
+var idade = 66;
+console.log(`você tem ${idade} anos.`)
 if (idade < 16) {
   console.log("Não vota");
-} else if (idade < 18) {
+} else if (idade < 18 || idade > 65) {
   // Se a idade for maior ou igual a 16 e menor que 18
   console.log("Voto opcional");
 } else {
