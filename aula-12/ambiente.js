c8baeb46b5b24103e46c86cc37563d5b76456142
Leonardo Gomes <devleonardo96@gmail.com>
2023-11-17 23:05:21 -0300

    att

---
+++ aula-12/ambiente.js
@@ -0,0 +1,13 @@
+// Inicializa a variável 'c' com o valor 1
+var c = 1;
+
+// Inicia um loop while com a condição 'c <= 6'
+while (c <= 6) {
+  // Imprime uma mensagem indicando o número de fatias de pizza com a variável 'c'
+  console.log(`Comi ${c} fatias de pizza!`);
+
+  // Incrementa a variável 'c' em 1 para a próxima iteração
+  c++;
+}
+
+// O loop termina quando 'c' é maior que 6, e o controle passa para o próximo bloco de código (se houver)
---
+++ aula-12/ambiente2.js
@@ -0,0 +1,15 @@
+// Inicializa a variável 'c' com o valor 1
+var c = 1;
+
+// Inicia um loop 'do-while'
+do {
+  // Imprime uma mensagem indicando o número de fatias de pizza com a variável 'c'
+  console.log(`Comi ${c} fatias de pizza!`);
+
+  // Incrementa a variável 'c' em 1 para a próxima iteração
+  c++;
+
+  // O loop continua enquanto 'c' é menor ou igual a 6
+} while (c <= 6);
+
+// O loop 'do-while' garante que o bloco de código seja executado pelo menos uma vez, mesmo que a condição seja falsa desde o início
