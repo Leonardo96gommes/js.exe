8a75b633f4b247e732f2820d2f8cdc91ae144ece
Leonardo Gomes <devleonardo96@gmail.com>
2023-11-20 22:43:50 -0300

    att

---
+++ aula-13/ambiente.js
@@ -0,0 +1,7 @@
+console.log("vai, começar...");
+// Utilizando um loop for para iterar de c = 1 até c <= 10
+for (var c = 1; c <= 10; c++) {
+  // Imprimindo uma mensagem no console indicando o número de fatias de pizza consumidas
+  console.log(`Comi ${c} fatias de pizza!`);
+}
+console.log("Fim!");
